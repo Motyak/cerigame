2d60417c1da2071b17f4dd5bdf44162a85831157
Anfosso Tommy <uapv1903668@pedago01c.univ-avignon.fr>
2020-10-09 12:48:00 +0200

    debut etape 2

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 const port = 3037;
-var path = require('path');
+const path = require('path');
 
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(session({
+    key: 'user_sid',
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 24 * 3600  //24h
+    }
+}));
+
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/test');
+    } else {
+        next();
+    }
+};
+
+app.get('/', sessionChecker, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'indexx.html'));
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    const mail = req.query.mail;
-    const pwd = req.query.pwd;
+app.post('/login', sessionChecker, (req, res) => {
+    // récupération des query strings
+    const mail = req.body.mail;
+    const pwd = req.body.pwd;
+
+    // ouverture session
+    req.session.user = mail;
+    console.log(req.session.id + ' expire dans ' + 
+        req.session.cookie.maxAge);
+
+    // on redirige vers l'index
+    res.sendFile(path.join(__dirname, 'public', 'test.html'));
+    
     console.log('mail :', mail, '\npwd :', pwd);
+});
+
+app.get('/test', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'test.html'));
 });
 
 app.listen(port, () => {
