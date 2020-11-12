56cba1c8d18345f2da34be9fc50aeb38722182df
Motyak <m0tyak_@live.fr>
2020-11-12 18:18:16 +0000

    auth service should be ok

--- frontend/src/app/authentification.service.ts
+++ frontend/src/app/authentification.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError, Subscriber } from 'rxjs';
+// import { catchError, retry } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthentificationService {
+
+  constructor(private http: HttpClient) {}
 
   isLogged(): boolean {
     // if(localStorage.getItem('accessToken'))
@@ -13,5 +19,23 @@ export class AuthentificationService {
     return false;
   }
 
-  constructor() { }
+  verifyId(user: string, pwd: string) : Observable<boolean> {
+    var trueId: boolean = false;
+
+    return Observable.create((observer: Subscriber<boolean>) => {
+      this.http.post<any>('api/endpoint', {user: user, pwd: pwd}).subscribe(
+        response => {
+          if(response.statusResp) {
+            localStorage.setItem('user', JSON.stringify(response.data.user));
+            trueId = true;
+          }
+        },
+        error => {
+          console.error('une erreur est survenue!', error);
+          trueId = false;
+        },
+        () => { observer.next(trueId); }
+      );
+    });
+  }
 }
