9f14053333cdddcdd790ef4c0c398f68f2886a0f
Motyak <m0tyak_@live.fr>
2020-12-13 20:15:32 +0000

    quizz OK

--- frontend/src/app/quizz/quizz.component.ts
+++ frontend/src/app/quizz/quizz.component.ts
@@ -12,12 +12,30 @@ export class QuizzComponent implements OnInit {
   diff: string;
   questionNb : number = 0;
 
+  answers : number[];
+
   constructor() { }
 
   ngOnInit(): void {
     this.quizz = JSON.parse(localStorage.getItem('quiz'));
     this.theme = localStorage.getItem('thème');
     this.diff = localStorage.getItem('diff');
+    this.answers = new Array(10);
   }
 
+  select(i : number) : void {
+    console.log(i + ' clicked');
+    // if(this.questionNb >= 9)
+    // {
+      // dire au composant principal que le quizz ne
+      // devrait plus s'afficher ?
+      // et afficher les résultats
+    // }
+
+    // if(this.questionNb < 9)
+    // {
+      this.answers[this.questionNb] = i;
+      ++this.questionNb;
+    // }
+  }
 }
