c0cf33c9f6d94544f6d7e19bc7060470ff4486dc
Motyak <m0tyak_@live.fr>
2020-12-14 04:09:05 +0000

    score fin

--- frontend/src/app/quizz/quizz.component.ts
+++ frontend/src/app/quizz/quizz.component.ts
@@ -15,11 +15,14 @@ export class QuizzComponent implements OnInit {
   diff: string;
   questionNb : number = 0;
   answers : number[];
-  
+
   interval;
   timer : string = '0:00';
   timerSec : number = 0;
   timerMin : number = 0;
+
+  nbGoodAnswers : number = 0;
+  score : number = 0;
 
   constructor() { }
 
@@ -49,6 +52,7 @@ export class QuizzComponent implements OnInit {
       if(this.questionNb == 10)
       {
         this.stopTimer();
+        this.calculateScore();
 
         // envoi des résultats à la bdd
       }
@@ -61,5 +65,43 @@ export class QuizzComponent implements OnInit {
 
   stopTimer() : void {
     clearInterval(this.interval);
+  }
+
+  calculateScore() : void {
+    const MAX_SCORE = 10000;
+
+    // 15s de temps de réponse en moyenne
+    const MAX_TIME = 150;
+
+    const MIN_TIME = {
+      'Facile': 100,    // 10s de temps de réponse en moyenne
+      'Normal': 40,     // 4s de temps de réponse en moyenne
+      'Difficile': 20   // 2s de temps de réponse en moyenne
+    };
+
+    // calcul nb de bonnes réponses
+    for(var i = 0 ; i < 10 ; ++i)
+    {
+      var a = this.answers[i];
+      if(this.quizz[i].quizz.réponse == this.quizz[i].quizz.propositions[a])
+        this.nbGoodAnswers++;
+    }
+
+    // calcul score par bonne réponse
+    var unitScore;
+
+    const TOTAL_TIME = this.timerMin * 60 + this.timerSec;
+
+    if(TOTAL_TIME < MIN_TIME[this.diff])
+      unitScore = MAX_SCORE / 10;
+    else
+    {
+      const SPEED_COEF = (TOTAL_TIME - MAX_TIME) 
+                      / (MIN_TIME[this.diff] - MAX_TIME);
+
+      unitScore = SPEED_COEF * (MAX_SCORE / 10);
+    }
+
+    this.score = Math.floor(this.nbGoodAnswers * unitScore);
   }
 }
