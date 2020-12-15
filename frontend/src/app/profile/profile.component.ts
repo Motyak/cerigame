aa59de01d08effa24ed4987907065fadd8f887ac
Motyak <m0tyak_@live.fr>
2020-12-15 05:46:18 +0000

    historique récupéré, pas encore affiché

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 enum Tab {
   PROFILE,
@@ -24,7 +25,9 @@ export class ProfileComponent implements OnInit {
   humeur: string;
   date_naissance: Date;
 
-  constructor() { }
+  history: any[];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     const username = localStorage.getItem('user');
@@ -37,22 +40,40 @@ export class ProfileComponent implements OnInit {
     this.avatar = profile.avatar;
     this.humeur = profile.humeur;
     this.date_naissance = profile.date_naissance;
+
+    this.getHistoryFromServer();
   }
 
   isSelected(tab: number) : boolean {
     return this.selectedTab == tab;
   }
 
-  loadProfile() : void {
+  selectProfile() : void {
     this.selectedTab = Tab.PROFILE;
   }
 
-  loadHistory() : void {
+  selectHistory() : void {
     this.selectedTab = Tab.HISTORY;
   }
 
   backToMenu() : void {
     // envoi msg au composant principal
     this.sendBackToMenuEmitter.emit();
+  }
+
+  getHistoryFromServer() : void {
+    const username = localStorage.getItem('user');
+    const user = JSON.parse(localStorage.getItem(username));
+    const idDb = user.idDb;
+
+    let params = new HttpParams().set("idDb", idDb);
+    this.http.get<any>('http://localhost:3037/histo', {params: params}).subscribe(
+      response => {
+        this.history = response;
+      },
+      error => {
+        console.log("err: l'historique n'a pas pu être récupéré");
+      }
+    );
   }
 }
